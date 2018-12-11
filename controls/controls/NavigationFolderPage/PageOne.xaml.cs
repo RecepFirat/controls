bf8d3aa81911acdb563bdcd609ca3d2926b53e21
recep tayyip fırat <rtf3444@gmail.com>
2018-12-12 02:16:45 +0300

    Add project files.

---
+++ controls/controls/NavigationFolderPage/PageOne.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace controls.NavigationFolderPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageOne : ContentPage
+	{
+		public PageOne ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void Button_Clicked(object sender, EventArgs e)//bekletme ıslemı async oldugundan fonk da async oluyo 
+
+        {
+            //await Navigation.PushModalAsync(new PageOne()); //iceriye yazdıgım async metodu awaitle bekletıyourum
+
+            //navigation mantıgında ıse
+            await Navigation.PushAsync(new PageTwo());
+        }
+    }
+}
---
+++ controls/controls/NavigationFolderPage/PageThree.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace controls.NavigationFolderPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageThree : ContentPage
+	{
+		public PageThree ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Uyari", "sen artık 3. sayfadasın", "Tamam");
+           await Navigation.PopAsync();//geriye gider
+        }
+    }
+}
